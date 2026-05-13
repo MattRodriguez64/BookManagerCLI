35bcaabfa5064f2c787c9bacafc0427cf15efd40
Matt R <matt.rodriguez64600@gmail.com>
2026-05-13 09:04:20 +0200

    Book class created and project structure created

---
+++ book.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+class Book:
+    def __init__(self, id:int, title:str, author:str, year:int, category:str, isAvailable: bool):
+        self.__id = id
+        self.__title = title
+        self.__author = author
+        self.__year = year
+        self.__category = category
+        self.__isAvailable = isAvailable
+
+    def __str__(self):
+        return f"{self.get_id()} : {self.get_title()} : {self.get_author()} : {self.get_year()} : {self.get_category()} : {self.get_isAvailable()}"
+    
+    def __repr__(self):
+        return f"Book({self.get_id()}, {self.get_title()}, {self.get_author()}, {self.get_year()}, {self.get_category()}, {self.get_isAvailable()})"
+
+    def get_id(self) -> int: return self.__id
+    def set_id(self, newId:int) -> None: self.__id = newId
+
+    def get_title(self) -> str: return self.__title
+    def set_title(self, newTitle:str) -> None: self.__title = newTitle
+
+    def get_author(self) -> str: return self.__author
+    def set_author(self, newAuthor:str) -> None: self.__author = newAuthor
+
+    def get_year(self) -> int: return self.__year
+    def set_year(self, newYear:int) -> None: self.__year = newYear
+
+    def get_category(self) -> str: return self.__category
+    def set_category(self, newCategory:str) -> None: self.__category = newCategory
+
+    def get_isAvailable(self) -> bool: return self.__isAvailable
+    def set_isAvailable(self, newIsAvailable:bool) -> None: self.__isAvailable = newIsAvailable
